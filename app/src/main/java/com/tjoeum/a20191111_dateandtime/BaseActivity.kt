7c03a017350b6d56ee84467028423050dd66d83d
Minsung89 <minsung0217@gmail.com>
2019-11-11 19:36:18 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeum/a20191111_dateandtime/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.tjoeum.a20191111_dateandtime
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity(){
+
+    abstract fun setupEvent()
+
+    abstract fun setValues()
+
+}
---
+++ app/src/main/java/com/tjoeum/a20191111_dateandtime/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.tjoeum.a20191111_dateandtime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvent() {
+    }
+
+    override fun setValues() {
+    }
+}
