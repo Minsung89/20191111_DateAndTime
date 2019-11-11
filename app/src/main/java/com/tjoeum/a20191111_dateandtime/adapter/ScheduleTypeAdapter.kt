b9350e3d273af6ef926390949eabb1252e268a01
Minsung89 <minsung0217@gmail.com>
2019-11-11 22:20:34 +0900

    완료

--- app/src/main/java/com/tjoeum/a20191111_dateandtime/adapter/ScheduleTypeAdapter.kt
+++ app/src/main/java/com/tjoeum/a20191111_dateandtime/adapter/ScheduleTypeAdapter.kt
@@ -1,10 +1,12 @@
 package com.tjoeum.a20191111_dateandtime.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.tjoeum.a20191111_dateandtime.R
 import com.tjoeum.a20191111_dateandtime.datas.ScheduleType
 
@@ -25,6 +27,27 @@ class ScheduleTypeAdapter (context: Context, res: Int, list: ArrayList<ScheduleT
 
         }
         var row = tempRow!!
+
+        return  row
+
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.schedule_type_list_item,null)
+
+        }
+        var row = tempRow!!
+
+        var summaryTxt = row.findViewById<TextView>(R.id.summaryTxt)
+        var typeTxt = row.findViewById<TextView>(R.id.typeTxt)
+
+        var data = mList.get(position)
+
+        typeTxt.text = data.title
+        summaryTxt.setBackgroundColor(Color.parseColor(data.color))
 
         return  row
 
